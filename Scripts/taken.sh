768234ed515a05a761bc7b70d59a1ce49b88bffe
Carlos <carlos.martinezg@estudiants.urv.cat>
2022-11-10 20:29:17 +0100

    CHANGES IN THE VALUES

--- Scripts/taken.sh
+++ Scripts/taken.sh
@@ -19,11 +19,9 @@ dir=$root"ammp/taken"
 out=$dir"/TAKEN_AMMP.txt"
 tasca="takenAMMP.txt"
 echo "processing "$tasca
-sim-outorder -fastfwd 100000000 \
+sim-bpred -fastfwd 100000000 \
 -max:inst 100000000 \
 -bpred taken \
--$memWidth \
--$memLat \
 -redir:sim $dir/$tasca /lib/specs2000/ammp/exe/ammp.exe < ammp.in > $dir/ammp.out 2> $dir/ammp.err
 echo "Taken:" > $out
 echo -e "\n\t" >> $out
@@ -37,11 +35,9 @@ dir=$root"eon/taken"
 out=$dir"/TAKEN_EON.txt"
 tasca="takenEON.txt"
 echo "processing "$tasca
-sim-outorder -fastfwd 100000000 \
+sim-bpred -fastfwd 100000000 \
 -max:inst 100000000 \
 -bpred taken \
--$memWidth \
--$memLat \
 -redir:sim $dir/$tasca /lib/specs2000/eon/exe/eon.exe chair.control.cook chair.camera chair.surfaces chair.cook.ppm ppm pixels_out.cook > $dir/cook_log.out 2> $dir/cook_log.err
 echo "Taken:" > $out
 echo -e "\n\t" >> $out
@@ -54,11 +50,9 @@ dir=$root"equake/taken"
 out=$dir"/TAKEN_EQUAKE.txt"
 tasca="takenEQUAKE.txt"
 echo "processing "$tasca
-sim-outorder -fastfwd 100000000 \
+sim-bpred -fastfwd 100000000 \
 -max:inst 100000000 \
 -bpred taken \
--$memWidth \
--$memLat \
 -redir:sim $dir/$tasca /lib/specs2000/equake/exe/equake.exe < inp.in > $dir/inp.out 2> $dir/inp.err	
 echo "Taken:" > $out
 echo -e "\n\t" >> $out
@@ -71,11 +65,9 @@ dir=$root"gap/taken"
 out=$dir"/TAKEN_GAP.txt"
 tasca="takenGAP.txt"
 echo "processing "$tasca
-sim-outorder -fastfwd 100000000 \
+sim-bpred -fastfwd 100000000 \
 -max:inst 100000000 \
 -bpred taken \
--$memWidth \
--$memLat \
 -redir:sim $dir/$tasca /lib/specs2000/gap/exe/gap.exe -l /usr/lib/specs2000/gap/data/all -q -m 192M < ref.in > $dir/ref.out 2> $dir/ref.err	
 echo "Taken:" > $out
 echo -e "\n\t" >> $out
@@ -88,11 +80,9 @@ dir=$root"mesa/taken"
 out=$dir"/TAKEN_MESA.txt"
 tasca="takenMESA.txt"
 echo "processing "$tasca
-sim-outorder -fastfwd 100000000 \
+sim-bpred -fastfwd 100000000 \
 -max:inst 100000000 \
 -bpred taken \
--$memWidth \
--$memLat \
 -redir:sim $dir/$tasca /lib/specs2000/mesa/exe/mesa.exe -frames 1000 -meshfile mesa.in -ppmfile mesa.ppm
 echo "Taken:" > $out
 echo -e "\n\t" >> $out
